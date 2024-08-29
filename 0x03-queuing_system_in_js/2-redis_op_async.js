0dd550483ee480d7c4042d2645bec70f3c1635b0
Cheikyadam <urbaingande@gmail.com>
2024-08-29 13:20:18 +0100

    2

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,35 @@
+import { createClient, print } from 'redis';
+const { promisify } = require('util');
+
+const client = createClient();
+
+function initialize(){
+  client.on('error', err => console.log(`Redis client not connected to the server: ${err.message}`))
+  client.on('connect', () => console.log('Redis client connected to the server'));
+}
+
+function setNewSchool(schoolName, value){
+  client.set(schoolName, value, print);
+}
+
+
+const getAsync = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName){
+  try{
+    const reply = await getAsync(schoolName);
+    console.log(`${reply}`);
+  }
+  catch (err) {
+    console.log(`Error: ${err.message}`);
+  } 
+}
+
+async function main(){
+  initialize();
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+}
+
+main();
